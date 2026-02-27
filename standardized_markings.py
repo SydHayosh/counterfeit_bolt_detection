e932a67183d3328bce1d9d3ef45a204b6d1eb652
Sydney Hayosh <sydney@hayosh.com>
2026-02-27 17:35:45 -0600

    Random stuff

--- standardized_markings.py
+++ standardized_markings.py
@@ -1,4 +1,16 @@
 import cv2 as cv
 
-print("hello world")
-print("test")
+img = cv.imread('Photos/ideal_bolt.jpg')
+
+cv.imshow('Ideal Bolt', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+canny = cv.Canny(img, 125, 175)
+cv.imshow('Canny Edges', canny)
+
+contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+print(f'{len(contours)} countours(s) found!')
+
+cv.waitKey(0)
