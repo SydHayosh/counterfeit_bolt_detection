2daa5e9735fa9989f48d5a4ae04c6476e70689cd
Sydney Hayosh <sydney@hayosh.com>
2026-03-12 13:54:25 -0500

    Added setup code.

--- image_generation.py
+++ image_generation.py
@@ -1,1 +1,13 @@
-import 
+from picamera2 import Picamera2
+import time
+
+camera = Picamera2()
+
+camera_config = camera.create_still_configuration()
+camera.configure(camera_config)
+
+camera.start()
+time.sleep(2)
+
+image_path = "/home/pi/captured_image.jpg"
+camera.capture_file(image_path)
