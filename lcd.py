7917500e971b7951d4dbc404acc12c2f8bd2399b
Sydney Hayosh <sydney@hayosh.com>
2026-04-07 19:46:06 -0500

    Two line display

--- lcd.py
+++ lcd.py
@@ -1,16 +1,28 @@
 import numpy as np
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 
 #button pins
 up = 10
 down = 11
 select = 12
 
-def setup():
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(up, GPIO.IN)
-    GPIO.setup(down, GPIO.IN)
-    GPIO.setup(select, GPIO.IN)
+# def setup():
+#     GPIO.setmode(GPIO.BOARD)
+#     GPIO.setup(up, GPIO.IN)
+#     GPIO.setup(down, GPIO.IN)
+#     GPIO.setup(select, GPIO.IN)
+
+def startTest():
+    print("Begin Test selected")
+
+def exportData():
+    print("Data Export selected")
+
+def settings():
+    print("Settings selected")
+
+def debugMenu():
+    print("Debug Menu selected")
 
 class MenuOption:
     def __init__(self, name, action = None):
@@ -26,14 +38,41 @@ topDisplayIndex = 0 #When the menu is greater then 2 options
 
 # functions
 def display_menu():
-    lcd.clear
-    lcd.write(0,0, mainMenu[topDisplayIndex].name)
-    lcd.write(0,0, mainMenu[topDisplayIndex+1].name)
+    # lcd.clear
+    # lcd.write(0,0, mainMenu[topDisplayIndex].name)
+    # lcd.write(0,0, mainMenu[topDisplayIndex+1].name)
+    if (currentIndex == topDisplayIndex):
+        print("->" + mainMenu[topDisplayIndex].name)
+        print(mainMenu[topDisplayIndex+1].name)
+    
+    else:
+        print(mainMenu[topDisplayIndex].name)
+        print("->" + mainMenu[topDisplayIndex+1].name)
 
 
 # add to menu
-mainMenu.append(MenuOption("Run Test"))
+mainMenu.append(MenuOption("Begin Test", startTest))
+mainMenu.append(MenuOption("Data Export", exportData))
+mainMenu.append(MenuOption("Settings", settings))
+mainMenu.append(MenuOption("Debug Menu", debugMenu))
 
 while True:
+    print("-----------------------\n\n\n")
     display_menu()
+    toggle = input()
+    while(toggle != "w" and toggle != "s"):
+        toggle = input()
+
+    
+    if(toggle == "s" and currentIndex+1 < len(mainMenu)):
+        currentIndex += 1
+        if(currentIndex > topDisplayIndex + 1):
+            topDisplayIndex += 1
+
+    elif(toggle == "w" and currentIndex > 0):
+        currentIndex -= 1
+        if(currentIndex < topDisplayIndex - 1):
+            topDisplayIndex -= 1
+            print(topDisplayIndex)
+    
     
