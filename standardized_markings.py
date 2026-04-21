6404a2d5c69793fa902fa58a5b4380c48756889f
Sydney Hayosh <sydney@hayosh.com>
2026-04-21 12:47:17 -0500

    Added circular mask

--- standardized_markings.py
+++ standardized_markings.py
@@ -1,96 +1,69 @@
 import numpy as np
 import cv2 as cv
 
-img = cv.imread('Photos/ideal_bolt_light_test.png')
-imgCT = cv.imread('Photos/counterfeit_bolt_titanium.jpg')
-imgCB = cv.imread('Photos/counterfeit_bolt_black.jpg')
+img = cv.imread('Photos/CBDS_Pics/ideal-pos20.jpg')
 
-# Canny edge thresholds
-lowerThresh = 125
-upperThresh = 175
+# ROI parameters
+center = (2205,1635) # true center is (2304,1296)
+radius = 170
+
+lowerThresh = 40
+upperThresh = 50
 
 if img is None:
     print("Image failed to load")
     exit()
 
-if imgCT is None:
-    print("Titanium image failed to load")
-    exit()
 
-if imgCB is None:
-    print("Black image failed to load")
-    exit()
-
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+#gray = cv.GaussianBlur(gray, (7, 7), 0)
 
 #cv.imshow('Ideal Bolt', img)
 #cv.imwrite("Photos/output/Ideal Bolt.jpg", img)
 
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Gray', gray)
-cv.imwrite("Photos/output/Gray.jpg", gray)
+preview = img.copy()
+cv.circle(preview, center, radius, (0,255,0), 3)
+cv.imshow("ROI Preview", preview)
 
-grayCT = cv.cvtColor(imgCT, cv.COLOR_BGR2GRAY)
-cv.imwrite('Photos/output/Gray Counterfeit Titanium.jpg', grayCT)
+# Create circular mask
+mask = np.zeros_like(gray, dtype=np.uint8)
+cv.circle(mask, center, radius, 255, -1)
 
-grayCB = cv.cvtColor(imgCB, cv.COLOR_BGR2GRAY)
-cv.imwrite('Photos/output/Gray Counterfeit Black.jpg', grayCB)
+# Apply mask
+maskedGray = cv.bitwise_and(gray, gray, mask=mask)
 
-# #gray = cv.GaussianBlur(gray, (7, 7), 0)
+# Edge detection ONLY in ROI
+cannyCircle = cv.Canny(maskedGray, lowerThresh, upperThresh)
+cannyCircle = cv.dilate(cannyCircle, None, iterations=1)
+cannyCircle = cv.erode(cannyCircle, None, iterations=1)
+
+cv.imshow("Edges in Circular ROI", cannyCircle)
+
+# Use THIS for contours (not full image!)
+#contours, hierarchies = cv.findContours(cannyCircle, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+contours, hierarchy = cv.findContours(cannyCircle, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+
+
 # blur = cv.medianBlur(grayCT,301) #number must be odd
 # cimg = imgCT.copy()
- 
-# circles = cv.HoughCircles(blur,cv.HOUGH_GRADIENT,1,20,param1=50,param2=32,minRadius=210,maxRadius=300)
 
-# if circles is not None: 
-#     circles = np.uint16(np.around(circles))
-#     for i in circles[0,:]:
-#         # draw the outer circle
-#         cv.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-#         # draw the center of the circle
-#         cv.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
-
-# else:
-#     print("No circles detected")
- 
-# cv.imshow('detected circles',cimg)
-# cv.imwrite("Photos/output/detected circles.jpg",cimg)
-
-canny = cv.Canny(gray, lowerThresh, upperThresh)
-#canny = cv.dilate(canny, None, iterations=1)
-#canny = cv.erode(canny, None, iterations=1)
-cv.imshow('Canny Edges', canny)
-cv.imwrite("Photos/output/Canny Edges.jpg", canny)
-
-cannyCT = cv.Canny(grayCT, lowerThresh, upperThresh)
-cv.imwrite("Photos/output/Canny Edges Counterfeit Titanium.jpg", cannyCT)
-
-cannyCB = cv.Canny(grayCB, lowerThresh, upperThresh)
-cv.imwrite("Photos/output/Canny Edges Counterfeit Black.jpg", cannyCB)
-
-contours, hierarchies = cv.findContours(canny, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-print(f'{len(contours)} countours(s) found on the ideal bolt!')
-hierarchies = hierarchies[0]
-
-contoursCT, hierarchiesCT = cv.findContours(cannyCT, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
-print(f'{len(contoursCT)} countours(s) found on the titanium bolt!')
-
-contoursCB, hierarchiesCB = cv.findContours(cannyCB, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
-print(f'{len(contoursCB)} countours(s) found on the black bolt!')
-
-markings = []
 
 idealBoltPhoto = img.copy()
-for i, contour in enumerate(contours):
+markings = []
+
+for contour in contours:
     area = cv.contourArea(contour)
 
-    if area > 50:
-        if hierarchies[i][3] == -1:
-            # outer contour
-            cv.drawContours(idealBoltPhoto, contours, i, (0,255,0), 3)
-        else: #elif area < 1500:
-            # inner contour
-            markings.append(contour)
-            cv.drawContours(idealBoltPhoto, contours, i, (0,0,255), 3)
+    if 120 < area < 130 or 107 < area < 109:
+        print(area)
+        markings.append(contour)
+        cv.drawContours(idealBoltPhoto, [contour], -1, (0, 255, 0), 10)
+        # if hierarchies[i][3] == -1:
+        #     # outer contour
+        #     cv.drawContours(idealBoltPhoto, contours, i, (0,255,0), 3)
+        # else: #elif area < 1500:
+        #     # inner contour
+            
 #cv.drawContours(idealBoltPhoto, contours, -1, (0,255,0), 10) #cv.drawContours(image being drawn on, contours, which contours to draw? just use -1, color, line thickness)
 cv.imshow('Contours', idealBoltPhoto)
 cv.imwrite("Photos/output/Contours on the Ideal Bolt.jpg", idealBoltPhoto)
@@ -99,3 +72,4 @@ print(f'\n There are {len(markings)} standardized markings')
 
 
 cv.waitKey(0)
+
