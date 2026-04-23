aad95da196ee0a3fc17dc3552b7ff50e82bf3ffe
SydHayosh <sydney@hayosh.com>
2026-04-23 01:08:54 -0500

    Fixed camera lens position

--- frameworkOIU/cameras.py
+++ frameworkOIU/cameras.py
@@ -4,11 +4,17 @@ def capture_photos():
     subprocess.run([
         "rpicam-still",
         "--camera", "0",
+        "--autofocus-mode", "manual",
+        "--lens-position", "20",
+        "--immediate",
         "-o", "frameworkOIU/inputCV/bolt_head.jpg"
     ])
 
     subprocess.run([
         "rpicam-still",
         "--camera", "1",
+        "--autofocus-mode", "manual",
+        "--lens-position", "32",
+        "--immediate",
         "-o", "frameworkOIU/inputCV/bolt_shaft.jpg"
     ])
