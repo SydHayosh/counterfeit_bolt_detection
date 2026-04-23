cbefd148eff6dee9f2cd3c6c574dcad6784dc9ae
SydHayosh <sydney@hayosh.com>
2026-04-23 00:03:04 -0500

    Update cameras.py

--- frameworkOIU/cameras.py
+++ frameworkOIU/cameras.py
@@ -1,28 +1,42 @@
-from picamera2 import Picamera2
-import time
-
-SAVE_DIR = "frameworksOIU/inputCV"
-
-camera_head = Picamera2(0)
-camera_shaft = Picamera2(1)
-
-cam_head_config = camera_head.create_still_configuration()
-camera_head.configure(cam_head_config)
-
-cam_shaft_config = camera_shaft.create_still_configuration()
-camera_shaft.configure(cam_shaft_config)
+import subprocess
 
 def capture_photos():
-    camera_head.start() # Maybe move start and stop to their own functions
-    camera_shaft.start()
+    subprocess.run([
+        "libcamera-still",
+        "--camera", "0",
+        "-o", "frameworksOIU/inputCV/bolt_head.jpg"
+    ])
 
-    time.sleep(2)
+    subprocess.run([
+        "libcamera-still",
+        "--camera", "1",
+        "-o", "frameworksOIU/inputCV/bolt_shaft.jpg"
+    ])
+# from picamera2 import Picamera2
+# import time
 
-    image_path_head = SAVE_DIR + "/bolt_head.jpg"
-    camera_head.capture_file(image_path_head)
+# SAVE_DIR = "frameworksOIU/inputCV"
 
-    image_path_shaft = SAVE_DIR + "/bolt_shaft.jpg"
-    camera_shaft.capture_file(image_path_shaft)
+# camera_head = Picamera2(0)
+# camera_shaft = Picamera2(1)
 
-    camera_head.stop()
-    camera_shaft.stop()
+# cam_head_config = camera_head.create_still_configuration()
+# camera_head.configure(cam_head_config)
+
+# cam_shaft_config = camera_shaft.create_still_configuration()
+# camera_shaft.configure(cam_shaft_config)
+
+# def capture_photos():
+#     camera_head.start() # Maybe move start and stop to their own functions
+#     camera_shaft.start()
+
+#     time.sleep(2)
+
+#     image_path_head = SAVE_DIR + "/bolt_head.jpg"
+#     camera_head.capture_file(image_path_head)
+
+#     image_path_shaft = SAVE_DIR + "/bolt_shaft.jpg"
+#     camera_shaft.capture_file(image_path_shaft)
+
+#     camera_head.stop()
+#     camera_shaft.stop()
