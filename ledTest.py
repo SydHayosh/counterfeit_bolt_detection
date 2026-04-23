9ab93c409703f95da7998537a5b6ed9b0f482c2b
SydHayosh <sydney@hayosh.com>
2026-04-23 01:27:23 -0500

    Single color options

--- ledTest.py
+++ ledTest.py
@@ -18,6 +18,15 @@ def set_all(color):
     for i in range(NUM_PIXELS):
         pixels[i] = color
     pixels.show()
+    
+def ledWhite():
+    set_all((255, 255, 255)
+
+def ledRed():
+    set_all((255, 0, 0)
+
+def ledGreen():
+    set_all((0, 255, 0)
 
 def ledCheck():
     try:
