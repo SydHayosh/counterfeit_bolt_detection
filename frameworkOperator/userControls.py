89bb530c8788b5962ddf75bf726f312cf7da158a
SydHayosh <sydney@hayosh.com>
2026-04-28 14:20:33 -0500

    changes

--- frameworkOperator/userControls.py
+++ frameworkOperator/userControls.py
@@ -28,6 +28,8 @@ def updateMenu(newMenu):
     topDisplayIndex = 0
 
 def startUp():
+	lcd.cursor_pos(0,0)
+	lcd.write_string("Initializing...")
     # Reads the ideal bolt magnetic dataset
     global idealData
     #idealData = getIdealData(magCheck.acceptedNumOfStdDev)
