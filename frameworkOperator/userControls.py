338cc491f18ff36ba872df1d3da7ab9159209eea
Sydney Hayosh <sydney@hayosh.com>
2026-04-28 00:57:43 -0500

    Back up mainOIU to run the CV tests

--- frameworkOperator/userControls.py
+++ frameworkOperator/userControls.py
@@ -51,8 +51,8 @@ def startTest():
     lcd.cursor_pos = (0,0)#(row, col)
 
     if magResult:
-            lcd.write_string("Magnetics: Passed")
-            setRegion(TOP, GREEN)
+        lcd.write_string("Magnetics: Passed")
+        setRegion(TOP, GREEN)
     else:
         lcd.write_string("Magnetics: Failed")
         setRegion(TOP, RED)
