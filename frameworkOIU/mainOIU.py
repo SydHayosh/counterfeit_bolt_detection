f4f41f3cc6a1c30b16f899bbe593aab294f4fb1f
Sydney Hayosh <sydney@hayosh.com>
2026-04-27 20:34:31 -0500

    Added functions to test CV

--- frameworkOIU/mainOIU.py
+++ frameworkOIU/mainOIU.py
@@ -1,9 +1,18 @@
 from .cameras import capture_photos
 from .dimensions import countThreads
+from ledTest import ledCheck, setRegion, RED, GREEN, BLUE, WHITE, OFF, TOP, HEAD, SHAFT
 from .standardized_markings import standMarkCheck
 
+
+
 def runTests():
+    setRegion(HEAD, WHITE)
+    setRegion(SHAFT, WHITE)
     capture_photos()
-    return countThreads() or standMarkCheck()
+    countThreads()
+    standMarkCheck()
+    setRegion(HEAD, OFF)
+    setRegion(SHAFT, OFF)
+
 
 runTests()
